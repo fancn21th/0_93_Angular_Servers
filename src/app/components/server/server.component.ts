cf43ac15d0da6edf4c2cd35493666072731ce578
fancn <fanyj@yikeer.com>
2019-08-04 12:32:53 +0800

    ngStyle

--- src/app/components/server/server.component.ts
+++ src/app/components/server/server.component.ts
@@ -9,11 +9,17 @@ export class ServerComponent implements OnInit {
   serverId: number = 10;
   serverStatus: string = "offline";
 
-  constructor() {}
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+  }
 
   ngOnInit() {}
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === "online" ? "green" : "red";
   }
 }
