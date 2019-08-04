b0e6b8276dff5e8d4237c0e2f7e9db622f02ba8f
fancn <fanyj@yikeer.com>
2019-08-04 10:26:03 +0800

    data binding

--- src/app/components/servers/servers.component.ts
+++ src/app/components/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = "No server was created!";
+  serverName: string = "";
 
   constructor() {
     setTimeout(() => {
@@ -19,5 +20,9 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = "Server was created";
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
